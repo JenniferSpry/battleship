74e9e6add65bf251bc7dc62994f3868515f5ab1a
Julius Zeidler <julius@zeidlos.de>
2013-04-19 12:42:11 +0200

    Added highlight functionality

--- js/application.js
+++ js/application.js
@@ -3,8 +3,14 @@
     var mousePos = getMousePos(canvas, evt);
     $('.cursor-position').html(mousePos.x + 'x/' + mousePos.y + 'y');
     $('.active-field').html(coordsToField(mousePos.x, mousePos.y)[0] + ',' + coordsToField(mousePos.x, mousePos.y)[1]);
+  }, false);
+  canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
     highlightField(coordsToField(mousePos.x, mousePos.y)[0], coordsToField(mousePos.x, mousePos.y)[1]);
-  }, false);  
+  });
+
+  
+
   enableDebug();
   drawPlayGround(60);
   drawPlayGround(500);
