c86fe56bd60d4194c88bb14ce32b263941d871c9
Julius Zeidler <julius@zeidlos.de>
2013-04-19 17:05:08 +0200

    Refactored creation of ships.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,20 @@
+var shotsFired = 0;
+var currentPhase = 'Init';
+var hitsLanded = 0;
+var playerActive = 'Computer';
+var hitPoints;
+
+var shipPositions = [
+  {start:[1,5], end: [3,5]},
+  {start:[8,2], end: [8,5]},
+  {start:[1,9], end: [4,9]},
+  {start:[9,9], end: [9,10]},
+  {start:[13,5], end: [15,5]},
+  {start:[15,7], end: [15,10]},
+  {start:[20,9], end: [20,10]},
+  {start:[18,2], end: [18,5]}
+];
+
 (function() {
   canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
@@ -6,12 +23,14 @@
   }, false);
   canvas.addEventListener('click', function(evt) {
     var mousePos = getMousePos(canvas, evt);
-    highlightField(coordsToField(mousePos.x, mousePos.y)[0], coordsToField(mousePos.x, mousePos.y)[1]);
+    checkHit(coordsToField(mousePos.x, mousePos.y)[0], coordsToField(mousePos.x, mousePos.y)[1], hitPoints);
   });
-
-  
 
   enableDebug();
   drawPlayGround(60);
   drawPlayGround(500);
+
+  for (var i = 0; i < shipPositions.length; i++) {
+    hitPoints = createShip(shipPositions[i].start,shipPositions[i].end);
+  }
 })();
--- js/func/populate.js
+++ js/func/populate.js
@@ -1,18 +1,17 @@
+var hitPoints = [];
+
 function createShip (start,end) {
   if (end[0]>start[0]) {
     for (var i = 0; start[0] <= end[0]; start[0]++) {
       drawField(start[0], start[1],'rgba(0,0,0,0.6)');
+      hitPoints.push([start[0], start[1]]);
     }
   }
   if (end[1]>start[1]) {
     for (var i2=0;start[1] <= end[1]; start[1]++) {
       drawField(start[0], start[1],'rgba(0,0,0,0.6)');
+      hitPoints.push([start[0], start[1]]);
     }
   }
+  return hitPoints;
 }
-
-
-createShip([1,5],[3,5]);
-createShip([8,2],[8,5]);
-createShip([1,9],[4,9]);
-createShip([9,9],[9,10]);
